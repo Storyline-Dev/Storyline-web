820fe3dc80d0fb90446a634767155d2c0e8cc694
Joel McElwee <joel.k.mcelwee@gmail.com>
2022-07-08 21:04:14 -0400

    living the dream

--- StorylineWebApp/storylineapp/urls.py
+++ StorylineWebApp/storylineapp/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from .views import ClientListView
 from . import views
 
 urlpatterns = [
--- StorylineWebApp/storylineapp/views.py
+++ StorylineWebApp/storylineapp/views.py
@@ -1,7 +1,42 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Client
 
-from django.http import HttpResponse
-
+clients = [
+    {
+        'name': 'Elsie Reed',
+        'dob': '10-14-1989',
+        'email':'elsie.reed@gmail.com',
+        'gender':'Female',
+        'race':'white',
+        'profession':'student',
+        'start_date':'01-20-2021',
+        'last_session':'03-02-2022',
+        'session_count':'19',
+        'activity_status':'true',
+        'therapist':'Wally Walleye'
+    },
+    {
+        'name': 'Rebecca Watters',
+        'dob': '05-15-1997',
+        'email':'beccawatts@gmail.com',
+        'gender':'Female',
+        'race':'white',
+        'profession':'nurse',
+        'start_date':'01-20-2021',
+        'last_session':'05-02-2022',
+        'session_count':'25',
+        'activity_status':'true',
+        'therapist':'Wally Walleye'
+    }
+]
 
 def home(request):
-    return render(request, 'users/dashboard.html')
+    context = {
+        'clients': clients
+    }
+    return render(request, 'users/clinicianHome.html', context)
+
+class ClientListView(ListView):
+    model = Client
+    template_name = 'storyline/home.html'
